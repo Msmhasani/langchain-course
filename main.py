935987939aab7ed89a4bcbb63936dc28a08ebdac
Meisam Hasani <mh.meisamhasani@gmail.com>
2026-05-16 23:18:58 +0900

    add ollama and fix

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+import os
+load_dotenv()
+
 def main():
     print("Hello from langchain-course!")
+    info = """Sasanian Empire of Persia"""
 
+
+    summary_tmp = """given the information {info} I want you to create:
+        1. A one line summary
+        2. two interesting facts about them
+    """
+
+    summary_prompt_tmplt = PromptTemplate(input_variables=["info"], template=summary_tmp)
+
+    llm = ChatOpenAI(temperature=0, model="gpt-5")
+    # llm = ChatOllama(temperature=0, model="gemma3")
+    
+    chain = summary_prompt_tmplt | llm
+    response = chain.invoke(input={"info": info})
+    print(response)
+
+    # Save response to txt file
+    with open("response.txt", "w", encoding="utf-8") as f:
+        f.write(response.content)
 
 if __name__ == "__main__":
     main()
