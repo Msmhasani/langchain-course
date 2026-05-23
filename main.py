c201f6eb4e015422b3cba45297bbe7c827d93e99
Meisam Hasani <mh.meisamhasani@gmail.com>
2026-05-23 12:33:39 +0900

    response format

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
 import os
 from pprint import pprint
 from dotenv import load_dotenv
@@ -13,6 +18,19 @@ from tavily import TavilyClient
 load_dotenv()
 
 tavily = TavilyClient()
+
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer:str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer.")
+
+
 
 @tool
 def search (query: str) -> str:
@@ -30,7 +48,8 @@ def search (query: str) -> str:
 llm = ChatOpenAI(model="gpt-5")
 tools = [search]
 # tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+# agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
